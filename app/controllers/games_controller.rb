45f9d0fb4b5c0ab858bd68f43fd60e37edd3519d
Fadal EL ABDIOUI <elabdioui.fadal@gmail.com>
2020-08-10 22:05:59 +0200

    Longest Word Game Finished

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,27 +1,30 @@
+require "open-uri"
+
 class GamesController < ApplicationController
+  VOWELS = %w(A E I O U Y)
+
   def new
-    @letters = ('A'..'Z').to_a.sample(10)
+    @letters = Array.new(5) { VOWELS.sample }
+    @letters += Array.new(5) { (('A'..'Z').to_a - VOWELS).sample }
+    @letters.shuffle!
   end
 
-  def score(attempt, grid)
-    if included?(attempt.upcase, grid)
-      if english_word?(attempt)
-        "well done"
-      else
-        "not an english word"
-      end
-    else
-      "not in the grid"
-    end
+  def score
+    @letters = params[:letters].split
+    @word = (params[:word] || "").upcase
+    @included = included?(@word, @letters)
+    @english_word = english_word?(@word)
   end
 
-  def included?(guess, grid)
-    guess.chars.all? { |letter| guess.count(letter) <= grid.count(letter) }
+  private
+
+  def included?(word, letters)
+    word.chars.all? { |letter| word.count(letter) <= letters.count(letter) }
   end
 
   def english_word?(word)
     response = open("https://wagon-dictionary.herokuapp.com/#{word}")
     json = JSON.parse(response.read)
-    return json['found']
+    json['found']
   end
 end
